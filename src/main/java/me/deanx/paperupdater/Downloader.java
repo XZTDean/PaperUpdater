c84437aec3d51a0e6702bff8983acf1bde9b6864
xztdean <xztdean@gmail.com>
2022-03-27 22:26:38 -0700

    Implement function to download jar file

--- src/main/java/me/deanx/paperupdater/Downloader.java
+++ src/main/java/me/deanx/paperupdater/Downloader.java
@@ -1,10 +1,23 @@
 package me.deanx.paperupdater;
 
+import java.io.IOException;
+import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Path;
 
 public class Downloader {
+    private HttpClient client;
     public Downloader() {
+        client = HttpClient.newHttpClient();
+    }
 
+    public void downloadJar(String url) throws IOException, InterruptedException {
+        HttpRequest request = HttpRequest.newBuilder(URI.create(url)).GET().build();
+        HttpResponse<Path> file = client.send(request, HttpResponse.BodyHandlers.ofFile(Path.of("./paper.jar")));
+        if (file.statusCode() >= 400) {
+            System.out.println("Error");
+        }
     }
 }
