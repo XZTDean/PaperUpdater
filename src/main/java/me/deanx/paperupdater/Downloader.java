1437deb37085b6b9cace074679841f3ee3d3d90c
xztdean <xztdean@gmail.com>
2022-04-25 23:19:18 -0700

    Add functions to get builds for a version

--- src/main/java/me/deanx/paperupdater/Downloader.java
+++ src/main/java/me/deanx/paperupdater/Downloader.java
@@ -1,5 +1,9 @@
 package me.deanx.paperupdater;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
@@ -8,7 +12,10 @@ import java.net.http.HttpResponse;
 import java.nio.file.Path;
 
 public class Downloader {
-    private HttpClient client;
+    private static final String BASE_URL = "https://papermc.io/api/v2/projects/paper/";
+    private final HttpClient client;
+    private final Gson gson = new Gson();
+
     public Downloader() {
         client = HttpClient.newHttpClient();
     }
@@ -19,5 +26,16 @@ public class Downloader {
         if (file.statusCode() >= 400) {
             System.out.println("Error");
         }
+    }
+
+    public int[] getVersionBuilds(String version) throws IOException, InterruptedException {
+        String url = BASE_URL + version;
+        HttpRequest request = HttpRequest.newBuilder(URI.create(url)).GET().build();
+        HttpResponse<String> buildInfo = client.send(request, HttpResponse.BodyHandlers.ofString());
+        if (buildInfo.statusCode() >= 400) {
+            System.out.println("Error");
+        }
+        JsonObject jsonObject = JsonParser.parseString(buildInfo.body()).getAsJsonObject();
+        return gson.fromJson(jsonObject.get("builds"), int[].class);
     }
 }
