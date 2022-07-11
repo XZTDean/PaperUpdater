3bd5203cd8bbfa758ba7661a307a8c4e92c726b1
XZTDean <xztdean@gmail.com>
2022-07-10 23:01:52 -0700

    Add interact mode and refactor code

--- src/main/java/me/deanx/paperupdater/CommandLineMain.java
+++ src/main/java/me/deanx/paperupdater/CommandLineMain.java
@@ -2,39 +2,39 @@ package me.deanx.paperupdater;
 
 import org.apache.commons.cli.*;
 
+import java.util.Scanner;
+
 public class CommandLineMain {
-    public static void main(String[] args) {
-        final Option LIST_INFO = new Option("l", "list", false, "List the build or download url");
-        final Option BUILD = new Option("b", "build", true, "Specify the build number");
-        final Option VERSION = new Option("v", "version", true, "Specify the minecraft version");
-        final Option VERSION_FAMILY = new Option("f", "family", true, "Specify the minecraft version family");
-        final Option OUTPUT = new Option("o", "output", true, "Output file name and path");
-        final Option INTERACT = new Option("i", "interact", false, "Interact mode");
-        final Option HELP = new Option("h", "help", false, "Help page");
+    public static void main(String[] args) throws ParseException {
+        CommandLineMain main = new CommandLineMain(args);
 
-        Options options = new Options();
-        options.addOption(LIST_INFO);
-        options.addOption(BUILD);
-        options.addOption(VERSION);
-        options.addOption(VERSION_FAMILY);
-        options.addOption(OUTPUT);
-        options.addOption(INTERACT);
-        options.addOption(HELP);
-
-        CommandLineParser parser = new DefaultParser(false);
-        CommandLine commandLine;
-        try {
-            commandLine = parser.parse(options, args);
-        } catch (ParseException e) {
-            System.err.println("Parsing failed.\n" + e.getLocalizedMessage());
-            return;
-        }
-
-        if (commandLine.hasOption(HELP)) {
+        if (main.commandLine.hasOption(HELP)) {
             HelpFormatter formatter = new HelpFormatter();
             formatter.printHelp("PaperUpdate", options);
             return;
         }
+
+        DownloadController downloader;
+        if (main.commandLine.hasOption(INTERACT)) {
+            downloader = main.getDownloadControllerFromInput();
+        } else {
+            downloader = main.getDownloadControllerFromArgs();
+        }
+
+        if (main.isDownload) {
+            downloader.download();
+        } else {
+            System.out.println("The download URL for version " + downloader.getCalculatedVersion()
+                    + " build " + downloader.getCalculatedBuild() + " is:\n" + downloader.getUrl());
+        }
+    }
+
+    private CommandLineMain(String[] args) throws ParseException {
+        CommandLineParser parser = new DefaultParser(false);
+        commandLine = parser.parse(options, args);
+    }
+
+    private DownloadController getDownloadControllerFromArgs() {
         DownloadController downloader = new DownloadController();
         if (commandLine.hasOption(OUTPUT)) {
             downloader.setOutputFile(commandLine.getOptionValue(OUTPUT));
@@ -48,12 +48,57 @@ public class CommandLineMain {
         if (commandLine.hasOption(BUILD)) {
             downloader.setBuild(Integer.parseInt(commandLine.getOptionValue(BUILD)));
         }
-
         if (commandLine.hasOption(LIST_INFO)) {
-            System.out.println("The download URL for version " + downloader.getCalculatedVersion()
-                    + " build " + downloader.getCalculatedBuild() + " is:\n" + downloader.getUrl());
-        } else {
-            downloader.download();
+            isDownload = false;
         }
+        return downloader;
+    }
+
+    private DownloadController getDownloadControllerFromInput() {
+        DownloadController downloader = new DownloadController();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Minecraft Version: ");
+        downloader.setVersion(scanner.nextLine());
+        System.out.println("Minecraft Version Family: ");
+        downloader.setVersionFamily(scanner.nextLine());
+        System.out.println("Build (blank for latest): ");
+        String build = scanner.nextLine();
+        if (!build.isBlank()) {
+            int buildNum = Integer.parseInt(build);
+            downloader.setBuild(buildNum);
+        }
+        System.out.println("Operation (0: Download, 1: Display Info): ");
+        if (scanner.nextInt() == 1) {
+            isDownload = false;
+        }
+        if (isDownload) {
+            System.out.println("Output File Location: ");
+            downloader.setOutputFile(scanner.nextLine());
+        }
+        scanner.close();
+        return downloader;
+    }
+
+    private final CommandLine commandLine;
+    private boolean isDownload = true;
+
+    private static final Options options = new Options();
+
+    private static final Option LIST_INFO = new Option("l", "list", false, "List the build or download url");
+    private static final Option BUILD = new Option("b", "build", true, "Specify the build number");
+    private static final Option VERSION = new Option("v", "version", true, "Specify the minecraft version");
+    private static final Option VERSION_FAMILY = new Option("f", "family", true, "Specify the minecraft version family");
+    private static final Option OUTPUT = new Option("o", "output", true, "Output file name and path");
+    private static final Option INTERACT = new Option("i", "interact", false, "Interact mode");
+    private static final Option HELP = new Option("h", "help", false, "Help page");
+
+    static {
+        options.addOption(LIST_INFO);
+        options.addOption(BUILD);
+        options.addOption(VERSION);
+        options.addOption(VERSION_FAMILY);
+        options.addOption(OUTPUT);
+        options.addOption(INTERACT);
+        options.addOption(HELP);
     }
 }
